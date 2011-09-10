e9536bcc26a73ba1e3dc5c1d4739e1fc9f85fcf3
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2011-09-09 18:14:26 -0600

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoteHive::Application.config.secret_token = 'b9ad12eadff137534b9bd45ae5630b5b5e2c1b6131374e4d4295fea646191e6d07378f7a0b04754b0b2a735a0772483b475a2e6c644dcfbf647a95addcccb86a'
