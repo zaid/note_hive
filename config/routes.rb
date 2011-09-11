8851436d125f1d7207e8ade03be7ca3396c1842b
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2011-09-11 12:02:24 -0600

    Add a Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 NoteHive::Application.routes.draw do
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
+  match '/signup', :to => 'users#new'
 
   root :to => 'pages#home'
 
