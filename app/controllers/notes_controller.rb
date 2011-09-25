b9210b3e9b8df5216cc7c45272d7518a902c3259
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2011-09-25 12:35:56 -0600

    Refactor the Notes controller with filters

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,14 +1,13 @@
 class NotesController < ApplicationController
   before_filter :authenticate
+  before_filter :get_notebook
   
   def new
     @title = 'New note'
-    @notebook = Notebook.find(params[:notebook_id])
     @note = @notebook.notes.build
   end
 
   def create
-    @notebook = current_user.notebooks.find(params[:notebook_id])
     @note = @notebook.notes.build(params[:note])
     @note.user = current_user
 
@@ -21,18 +20,15 @@ class NotesController < ApplicationController
   end
 
   def show
-    @notebook = Notebook.find(params[:notebook_id])
     @note = @notebook.notes.find(params[:id])
   end
 
   def edit
     @title = 'Edit note'
-    @notebook = current_user.notebooks.find(params[:notebook_id])
     @note = @notebook.notes.find(params[:id])
   end
 
   def update
-    @notebook = current_user.notebooks.find(params[:notebook_id])
     @note = @notebook.notes.find(params[:id])
     
     if @note.update_attributes(params[:note])
@@ -45,10 +41,15 @@ class NotesController < ApplicationController
   end
 
   def destroy
-    @notebook = Notebook.find(params[:notebook_id])
     @note = @notebook.notes.find(params[:id])
     @note.destroy
     redirect_to @notebook
+  end
+
+  private
+
+  def get_notebook
+    @notebook = current_user.notebooks.find(params[:notebook_id])
   end
 
 end
